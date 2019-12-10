176e00e04c4c9412a9979cda63403beae22014b7
kunkliricsi <kunkli.ricsi@gmail.com>
2019-12-10 09:34:35 +0100

    Added automatic settings upgrade

--- MarkAllAsReadAddIn/MarkAllAsReadRibbon.cs
+++ MarkAllAsReadAddIn/MarkAllAsReadRibbon.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MarkAllRead.Properties;
 using Outlook = Microsoft.Office.Interop.Outlook;
 
 namespace MarkAllRead
@@ -16,7 +17,14 @@ namespace MarkAllRead
 
         private void MarkAllAsRead_Load(object sender, RibbonUIEventArgs e)
         {
-            var storedFolders = Properties.Settings.Default.SelectedFolders;
+            if (Settings.Default.UpgradeRequired)
+            {
+                Settings.Default.Upgrade();
+                Settings.Default.UpgradeRequired = false;
+                Settings.Default.Save();
+            }
+
+            var storedFolders = Settings.Default.SelectedFolders;
 
             _form = new DialogLauncher(Globals.ThisAddIn.Application, storedFolders?.Cast<string>());
             _folders = _form.SelectedFolders;
@@ -28,9 +36,9 @@ namespace MarkAllRead
             if (result == DialogResult.OK)
             {
                 _folders = _form.SelectedFolders;
-                Properties.Settings.Default.SelectedFolders = new StringCollection();
-                Properties.Settings.Default.SelectedFolders.AddRange(_folders.Select(f => f.Name).ToArray());
-                Properties.Settings.Default.Save();
+                Settings.Default.SelectedFolders = new StringCollection();
+                Settings.Default.SelectedFolders.AddRange(_folders.Select(f => f.Name).ToArray());
+                Settings.Default.Save();
             }
         }
 
@@ -38,7 +46,7 @@ namespace MarkAllRead
         {
             if (!_folders.Any())
             {
-                MessageBox.Show("No folder is selected.", "Mark All As Read", MessageBoxButtons.OK);
+                groupMarkAll_DialogLauncherClick(sender, e);
                 return;
             }
 
--- MarkAllAsReadAddIn/Properties/Settings.Designer.cs
+++ MarkAllAsReadAddIn/Properties/Settings.Designer.cs
@@ -33,5 +33,17 @@ namespace MarkAllRead.Properties {
                 this["SelectedFolders"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        public bool UpgradeRequired {
+            get {
+                return ((bool)(this["UpgradeRequired"]));
+            }
+            set {
+                this["UpgradeRequired"] = value;
+            }
+        }
     }
 }
